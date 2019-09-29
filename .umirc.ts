b746d91d3cd1a615b8c88235a1166b0f82dffcdc
曹德博 <13335137159@163.com>
2019-09-29 23:03:20 +0800

    修改打包配置

--- .umirc.ts
+++ .umirc.ts
@@ -7,29 +7,6 @@ const config: IConfig = {
     htmlSuffix: true,
   },
 
-  chainWebpack: function (config, { webpack }) {
-    config.merge({
-      optimization: {
-        minimize: true,
-        splitChunks: {
-          chunks: 'all',
-          minSize: 30000,
-          minChunks: 3,
-          automaticNameDelimiter: '.',
-          cacheGroups: {
-            vendor: {
-              name: 'vendors',
-              test({ resource }) {
-                return /[\\/]node_modules[\\/]/.test(resource);
-              },
-              priority: 10,
-            },
-          },
-        },
-      }
-    });
-  },
-
   plugins: [
     ['umi-plugin-react', {
       antd: true,
@@ -37,7 +14,6 @@ const config: IConfig = {
         hmr:true
       },
       pwa:true,
-      chunks: ['vendors', 'umi'],
       dynamicImport: false,
       title: 'Website-React',
       routes: {
